a7735a6b046d3b3fbd02086b2a112359fe171e9e
Nodev2-alt <node.xyz.farcaster@gmail.com>
2026-05-08 04:10:32 +0100

    Add requirements and config

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-FACTORY_ADDRESS = "0x273b8fc139617f856fd3da0b89c29912a381901d"
-RPC_URL = "https://sepolia.base.org"
-CHAIN_ID = 84532
-PLATFORM_WALLET = "0xab0f481fcae15f76af749b6adb699cf5566b45b6"
-DEPLOY_FEE = 500000000000000  # 0.0005 ETH in wei
+import os
+
+FACTORY_ADDRESS  = "0x273b8fc139617f856fd3da0b89c29912a381901d"
+RPC_URL          = "https://sepolia.base.org"
+CHAIN_ID         = 84532
+PLATFORM_WALLET  = os.environ.get("PLATFORM_WALLET", "0xab0f481fcae15f76af749b6adb699cf5566b45b6")
+DEPLOY_FEE       = 500000000000000
+PRIVATE_KEY      = os.environ.get("PRIVATE_KEY", "0xb3e3dbbac8c78c0269a1a083bec0f2043bb6fee8ebe80c55ac0174828a1a2262")
